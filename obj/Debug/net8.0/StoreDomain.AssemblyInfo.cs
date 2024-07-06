c77c1905ea17eb9a56defc9c5402b4b5c86f133c
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-07-06 12:56:44 -0300

    Entity base class

--- obj/Debug/net8.0/StoreDomain.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreDomain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a4d90657605066be911aad0099d5612091a927")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964b51f7cb6ce2bbbf75aca3894a88469a5103d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
